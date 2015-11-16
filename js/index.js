8786a5173165253739340f5a93fb940bb92a0587
Dmitrij Bar <baradmitrij@yandex.ru>
2015-11-16 17:13:47 +0200

    added js-button to scroll Up

--- js/index.js
+++ js/index.js
@@ -11,3 +11,40 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   document.querySelector('head').appendChild(msViewportStyle)
 }
 /* fix of Internet Explorer 10 in Windows 8 and Windows Phone 8*/
+
+window.onload = function () {
+
+  /*Кнопка прокрутить вверх*/
+
+// сама кнопка с документом
+  var html = document.documentElement;
+  var body = document.body;
+  var btnUp = document.querySelector(".btnUp");
+// сама кнопка
+
+// функция скролла вверх
+  function toScrollTop() {
+    $('html, body').animate({scrollTop: 0}, 500);
+    return false;
+  };
+// функция скролла вверх
+
+// показ кнопки прокрутить вверх
+  $(window).scroll(function () {
+    var scrollTop = html.scrollTop || body && body.scrollTop || 0;
+    scrollTop -= html.clientTop;
+    if (scrollTop > 500) {
+      btnUp.style.display = "block";
+    } else {
+      btnUp.style.display = "none";
+    }
+  });
+// показ кнопки прокрутить вверх
+
+// выполнить действие проскролить вверх
+  $(btnUp).click(toScrollTop);
+// выполнить действие проскролить вверх
+
+  /*Кнопка прокрутить вверх*/
+
+};
